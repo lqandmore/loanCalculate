7224faf3e7fe40f3f52ee664240d78d3f99cfbd0
liquan <lqandmore@163.com>
2023-03-22 21:32:47 +0800

    Update login-panal.vue

--- src/components/login-panal.vue
+++ src/components/login-panal.vue
@@ -3,12 +3,13 @@ const a = 0
 const b = 1
 const c = a + b
 
-const d = 
-
-
+const d = 0 + c
 </script>
 <template>
-  <div></div>
+  <div>
+    <h2>hahahh</h2>
+    <a href="www.baidu.com"></a>
+  </div>
 </template>
 <style scoped>
 </style>
