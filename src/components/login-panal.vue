82f25d4d95b52e1ddf979515fa99ddaf58422492
liquan <lqandmore@163.com>
2023-03-22 21:33:44 +0800

    Update login-panal.vue

--- src/components/login-panal.vue
+++ src/components/login-panal.vue
@@ -6,9 +6,10 @@ const c = a + b
 const d = 0 + c
 </script>
 <template>
-  <div>
+  <div class="login-panel">
     <h2>hahahh</h2>
-    <a href="www.baidu.com"></a>
+    <a href="www.baidu.com">百度</a>
+    
   </div>
 </template>
 <style scoped>
